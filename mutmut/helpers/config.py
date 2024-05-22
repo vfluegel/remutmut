e1141a84fe85c483942ec4cdf8d7e8ed1549ddd0
vanessaflugel <vanessa.fluegel@gmx.de>
2024-05-22 18:04:02 +0200

    Move config_from_file from config to cli utils

--- mutmut/helpers/config.py
+++ mutmut/helpers/config.py
@@ -1,8 +1,5 @@
-from configparser import ConfigParser
 from dataclasses import dataclass, field
-from functools import wraps
 from typing import Optional, Dict, List, Set
-import toml
 
 
 @dataclass
@@ -30,34 +27,3 @@ class Config:
 
     def __post_init__(self):
         self._default_test_command = self.test_command
-
-
-def config_from_file(**defaults):
-    def config_from_pyproject_toml() -> dict:
-        try:
-            return toml.load('pyproject.toml')['tool']['mutmut']
-        except (FileNotFoundError, KeyError):
-            return {}
-
-    def config_from_setup_cfg() -> dict:
-        config_parser = ConfigParser()
-        config_parser.read('setup.cfg')
-
-        try:
-            return dict(config_parser['mutmut'])
-        except KeyError:
-            return {}
-
-    config = config_from_pyproject_toml() or config_from_setup_cfg()
-
-    def decorator(f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            for k in list(kwargs.keys()):
-                if not kwargs[k]:
-                    kwargs[k] = config.get(k, defaults.get(k))
-            f(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
